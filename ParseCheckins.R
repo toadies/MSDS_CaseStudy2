e3cb6644dc5281f3194e4edfe95cd5b354a67bcf
Chris <chris.ballenger@gmail.com>
2018-08-05 18:58:01 -0500

    Parse JSON objects

---
+++ ParseCheckins.R
@@ -0,0 +1,94 @@
+setwd("/Users/christopherballenger/Documents/Data Science/MSDS 6306/Projects/CaseStudy2")
+
+library(jsonlite)
+library(plyr)
+
+jsonObjects <- list.files("Responses/")
+i = 1
+source("Untappd_api.R")
+# result <- data.frame(
+#     breweryId = numeric(),
+#     breweryName = character(),
+#     beerId = numeric(),
+#     beerName = character(),
+#     beerStyle = character(),
+#     beerAbv = numeric(),
+#     beerIbu = numeric(),
+#     checkinId = numeric(),
+#     createdAt = character(),
+#     ratingScore = numeric()
+# )
+result <- data.frame()
+# filterObjects <- jsonObjects[grep("*beer.checkins*",jsonObjects)]
+filterObjects <- jsonObjects[grep("*son",jsonObjects)]
+while( i <= length(filterObjects) ){
+    # if( length(grep("*beer.checkins*",jsonObjects[i] )) >= 1 ){
+    checkins <- fromJSON(paste("Responses/", filterObjects[i], sep=""))
+    if( checkins$response$checkins$count > 0 ){
+        result <- rbind( result, readBeerCheckins(checkins) )
+    }
+    # }
+    print( paste(i, "-", dim(result)[1] ) )
+    i = i + 1
+}
+
+# a list in which each element is one of the JSON files
+# result <- lapply(
+#     filterObjects,
+#     function(x){
+#         print(x)
+#         readBeerCheckins(
+#             fromJSON(paste("Responses/", x, sep=""))
+#         )
+#     }
+# )
+# result <- do.call(rbind.data.frame, result)
+# write.csv(result,"checkins1.csv")
+
+# dim(result)
+# str(result)
+result.distinct <- unique(result)
+beerIds <- plyr::count(result.distinct, "beerId")
+distinctBeerIds <- beerIds[order(beerIds$freq, decreasing = F),]
+# distinctBeerIds
+# dim(result)
+# dim(result.distinct)
+str(result.distinct)
+write.csv(result.distinct, "checkins.csv")
+
+maxIds <- aggregate( result.distinct$checkinId, list( result.distinct$beerId ), max )
+minIds <- aggregate( result.distinct$checkinId, list( result.distinct$beerId ), min )
+lastCreatedDate <- aggregate( 
+    strptime( result.distinct$createdAt, "%a, %d %b %Y %T"),
+    list(result.distinct$beerId ), 
+    max,
+    na.rm=TRUE 
+)
+
+breweries <- unique(result.distinct[,1:4])
+str(breweries)
+breweries <- merge(breweries, distinctBeerIds, by.x = "beerId", by.y = "beerId" )
+breweries <- merge(breweries, maxIds, by.x = "beerId", by.y = "Group.1")
+breweries <- merge(breweries, minIds, by.x = "beerId", by.y = "Group.1")
+breweries <- merge(breweries, lastCreatedDate, by.x = "beerId", by.y = "Group.1")
+names(breweries) <- c("beerId","breweryId","breweryName","beerName","totalCheckins","maxId","minId", "lastCreatedDate")
+
+write.csv(breweries, "breweries.csv",row.names = FALSE)
+
+head(breweries)
+
+# 15663
+# 15688
+# 17813
+# 19698
+# 22797
+# 24975
+# 27129
+# 29252
+# 31277
+# 33268
+# 35182
+# 35275
+# 36879
+# 38053
+dim(result.distinct)
--- Untappd_api.R
+++ Untappd_api.R
@@ -16,6 +16,7 @@ fetchUntappd <- function(method = "", qryStr = "", writeFile = FALSE, fileName =
         }
         
         fileName <- paste( "Responses", fileName, sep="/" )
+        print(fileName)
         write( content( response, "text"), fileName) 
     }
     response
@@ -38,7 +39,7 @@ getBreweryCheckIns <- function(breweryId, maxId = 0,  writeFile = FALSE){
         qryStr <- paste("max_id=", maxId, sep="")
     } else {
         # default the fileName maxId to 999999999
-        maxId = 999999999
+        maxId = as.numeric(Sys.time())*1000
     }
         
     response <- fetchUntappd( 
@@ -56,7 +57,7 @@ getBeerCheckIns <- function(beerId, maxId = 0, writeFile = FALSE){
         qryStr <- paste("max_id=", maxId, sep="")
     } else {
         # default the fileName maxId to 999999999
-        maxId = 999999999
+        maxId = as.numeric(Sys.time())*1000
     }
     
     response <- fetchUntappd( 
@@ -64,5 +65,112 @@ getBeerCheckIns <- function(beerId, maxId = 0, writeFile = FALSE){
         qryStr, 
         writeFile, 
         fileName = paste( "beer.checkins", beerId, maxId, "json",sep="." )
+    )
+}
+
+# getVenueInfo <- function(venueId, writeFile = FALSE){
+#     pathStr <- paste("venue/info/",venueId,sep="")
+#     response <- fetchUntappd( 
+#         pathStr,
+#         writeFile, 
+#         fileName = paste( "venue>info", venueId, "json",sep="." )
+#     )
+# }
+
+readBeerCheckins <- function(checkins = list()){
+    items <- checkins$response$checkins$items
+    # Loop Through Venue Data since its a list
+    venueId <- numeric()
+    venueName <- character()
+    venueParentCategory <- character()
+    venueParentCategoryId <- character()
+    venueCategory <- character()
+    venueCategoryId <- character()
+    venueCategoryIsPrimary <- logical()
+    venueCity <- character()
+    venueState <- character()
+    venueCountry <- character()
+    venueLat <- numeric()
+    venueLng <- numeric()
+    venueFourSquareId <- character()
+    venueFourSquareUrl <- character()
+    for(i in 1:checkins$response$checkins$count){
+        
+        if(class(checkins$response$checkins$items[i,]$venue) == "data.frame"){
+            venue <- checkins$response$checkins$items[i,]$venue
+        } else {
+            venue <- checkins$response$checkins$items[i,]$venue[[1]]
+        }
+        
+        if( length(checkins$response$checkins$items[i,]$venue[[1]]) > 0 ){
+            venueId[i] <- venue$venue_id
+            venueName[i] <- venue$venue_name
+            venueParentCategory[i] <- ifelse(!is.null(venue$primary_category),venue$primary_category,NA)
+            venueParentCategoryId[i] <- ifelse(!is.null(venue$parent_category_id),venue$parent_category_id,NA)
+            if(venue$categories$count > 0){
+                if(class(venue$categories$items) == "data.frame"){
+                    categories <- venue$categories.items[1,]
+                } else {
+                    categories <- venue$categories$items[[1]]
+                }
+                venueCategory[i] <- ifelse(!is.null(categories$category_name),categories$category_name,NA)
+                venueCategoryId[i] <- ifelse(!is.null(categories$category_id),categories$category_id,NA)
+                venueCategoryIsPrimary[i] <- ifelse(!is.null(categories$is_primary),categories$is_primary,NA)
+            } else {
+                venueCategory[i] <- NA
+                venueCategoryId[i] <- NA
+                venueCategoryIsPrimary[i] <- NA
+            }
+            venueCity[i] <- ifelse(!is.null(venue$location$venue_city),venue$location$venue_city,NA)
+            venueState[i] <- ifelse(!is.null(venue$location$venue_state),venue$location$venue_state,NA)
+            venueCountry[i] <- ifelse(!is.null(venue$location$venue_country),venue$location$venue_country,NA)
+            venueLat[i] <- ifelse(!is.null(venue$location$lat),venue$location$lat,NA)
+            venueLng[i] <- ifelse(!is.null(venue$location$lng),venue$location$lng,NA)
+            venueFourSquareId[i] <- ifelse(!is.null(venue$foursquare$foursquare_id),venue$foursquare$foursquare_id,NA)
+            venueFourSquareUrl[i] <- ifelse(!is.null(venue$foursquare$foursquare_url),venue$foursquare$foursquare_url,NA)
+        } else {
+            venueId[i] <- NA
+            venueName[i] <- NA
+            venueParentCategory[i] <- NA
+            venueParentCategoryId[i] <- NA
+            venueCategory[i] <- NA
+            venueCategoryId[i] <- NA
+            venueCategoryIsPrimary[i] <- NA
+            venueCity[i] <- NA
+            venueState[i] <- NA
+            venueCountry[i] <- NA
+            venueLat[i] <- NA
+            venueLng[i] <- NA
+            venueFourSquareId[i] <- NA
+            venueFourSquareUrl[i] <- NA
+        }
+    }
+
+    data.frame(
+        breweryId = items$brewery$brewery_id,
+        breweryName = items$brewery$brewery_name,
+        beerId = items$beer$bid,
+        beerName = items$beer$beer_name,
+        beerDescription = items$beer$beer_description,
+        beerStyle = items$beer$beer_style,
+        beerAbv = items$beer$beer_abv,
+        beerIbu = items$beer$beer_ibu, 
+        checkinId = items$checkin_id,
+        createdAt = items$created_at,
+        ratingScore = items$rating_score,
+        venueId = venueId,
+        venueName = venueName,
+        venueParentCategory = venueParentCategory,
+        venueParentCategoryId = venueParentCategoryId,
+        venueCategory = venueCategory,
+        venueCategoryId = venueCategoryId,
+        venueCategoryIsPrimary = venueCategoryIsPrimary,
+        venueCity = venueCity,
+        venueState = venueState,
+        venueCountry = venueCountry,
+        venueLat = venueLat,
+        venueLng = venueLng,
+        venueFourSquareId = venueFourSquareId,
+        venueFourSquareUrl = venueFourSquareUrl
     )
 }
