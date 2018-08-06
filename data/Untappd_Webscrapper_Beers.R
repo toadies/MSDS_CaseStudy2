18d608b36ce9e57a21c81d3d737ff2415674dfaf
selwyn-samuel-smu <selwyns@mail.smu.edu>
2018-08-06 00:17:16 -0400

    Auto stash before merge of "master" and "origin/master"
    Moved files to data directory.

---
+++ data/Untappd_Webscrapper_Beers.R
@@ -0,0 +1,118 @@
+#
+# R Code to scrape the user checkin data from Untappd Website
+#
+library(RCurl)
+library(XML)
+library(ggplot2)
+library(httr)
+library(rvest) 
+library(kableExtra)
+library(tidyr)
+library(RSelenium)
+library(stringr)
+
+setwd('C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/')
+source("env.R")
+source("Untappd_functions.R")
+
+#
+# Given a user name, the function will return a data frame of all the user checkin details
+#
+get_beer_details <- function(file.name) {
+  
+  beer.details <- data.frame(matrix(ncol = 46, nrow = 0))
+  colnames(beer.details) <- c("BeerId", "BreweryName", "BreweryURL", "Style", "ABV", "IBU", "BeerRating", "TotalRatings", "TotalCheckins", "UniqueCheckins",
+                              "MonthlyCheckins", "Light", "Smooth", "Clean", "Sweet", "Woody", "Zippy", "Strong", "Hoppy", "Floral", "Citrus", "Milk", "Dark", "Creamy",
+                              "Mouthfeel", "Soft", "Boozy", "Caramel", "Oatmeal", "Dry", "Malty", "Piney", "Grassy", "Tart", "Sour", "Salty", "Fruity", "Heat", 
+                              "Bitter", "Coffee", "Roasty", "Chocolate", "Juicy", "Funky", "Acidic", "Crushable")
+  
+  beer.ids <- read.csv(file.name)
+  remDr <- get_remote_chrome_driver() 
+  
+  #for(row in 1:nrow(beer.ids)) {
+  for(row in 1:nrow(beer.ids)) {
+      
+    print(row)
+    tryCatch({
+      message("Processing Beer Id... ", beer.ids[row,]$BeerId)
+      newBeer <- data.frame(BeerId=beer.ids[row,]$BeerId, BreweryName="", BreweryURL="", Style="", ABV="0%", IBU=0, BeerRating=0, TotalRatings=0, TotalCheckins=0, UniqueCheckins=0,
+                            MonthlyCheckins=0, Light=0, Smooth=0, Clean=0, Sweet=0, Woody=0, Zippy=0, Strong=0, Hoppy=0, Floral=0, Citrus=0, Milk=0, Dark=0, Creamy=0, Mouthfeel=0, 
+                            Soft=0, Boozy=0, Caramel=0, Oatmeal=0, Dry=0, Malty=0, Piney=0, Grassy=0, Tart=0, Sour=0, Salty=0, Fruity=0, Heat=0, Bitter=0, Coffee=0, Roasty=0, 
+                            Chocolate=0, Juicy=0, Funky=0, Acidic=0, Crushable=0)
+      
+      beer.url <- paste(base.url, "beer/", beer.ids[row, "BeerId"], sep = "")
+      remDr$navigate(beer.url)
+      window.handle <- remDr$getCurrentWindowHandle()
+      
+      beer.page <- read_html(as.character(remDr$getPageSource()))
+      beer.detail.node <- html_nodes(beer.page, 'div[class="box b_info"]')
+      
+      newBeer$BreweryName <- html_text(html_nodes(beer.detail.node, 'div[class="name"] p[class="brewery"] a'))
+      newBeer$BreweryURL <- html_attr(html_nodes(beer.detail.node, 'div[class="name"] p[class="brewery"] a'), "href")
+      newBeer$Style <- html_text(html_nodes(beer.detail.node, 'div[class="name"] p[class="style"]'))
+      newBeer$ABV <- tail(unlist(strsplit(trimws(html_text(html_nodes(beer.detail.node, 'div[class="details"] p[class="abv"]'))), " ")), n=2)[1]
+      newBeer$IBU <- tail(unlist(strsplit(trimws(html_text(html_nodes(beer.detail.node, 'div[class="details"] p[class="ibu"]'))), " ")), n=2)[1]
+      newBeer$TotalRatings <- tail(unlist(strsplit(trimws(html_text(html_nodes(beer.detail.node, 'div[class="details"] p[class="raters"]'))), " ")), n=2)[1]
+      beer.rating.str <- html_attr(html_nodes(beer.detail.node, 'div[class="details"] p[class="rating"] span'), "class")[1]
+      newBeer$BeerRating <- as.numeric(substring(beer.rating.str, nchar(beer.rating.str)-2))/100
+      
+      counts.list <- html_text(html_nodes(beer.detail.node, 'div[class="stats"] p span[class="count"]'))
+      newBeer$TotalCheckins <- counts.list[1]
+      newBeer$UniqueCheckins <- counts.list[2]
+      newBeer$MonthlyCheckins <- counts.list[3]
+      
+      beer.description <- strsplit(trimws(html_text(html_nodes(beer.detail.node, 'div[class="bottom"] div[class="desc"] div[class="beer-descrption-read-less"]'))), " ")
+      if(length(beer.description) > 0) {
+        if(str_detect(beer.description, fixed("Light", ignore_case=TRUE))) { newBeer$Light = 1 }
+        if(str_detect(beer.description, fixed("Smooth", ignore_case=TRUE))) { newBeer$Smooth = 1 }
+        if(str_detect(beer.description, fixed("Clean", ignore_case=TRUE))) { newBeer$Clean = 1 }
+        if(str_detect(beer.description, fixed("Sweet", ignore_case=TRUE))) { newBeer$Sweet = 1 }
+        if(str_detect(beer.description, fixed("Woody", ignore_case=TRUE))) { newBeer$Woody = 1 }
+        if(str_detect(beer.description, fixed("Zippy", ignore_case=TRUE))) { newBeer$Zippy = 1 }
+        if(str_detect(beer.description, fixed("Strong", ignore_case=TRUE))) { newBeer$Strong = 1 }
+        if(str_detect(beer.description, fixed("Hoppy", ignore_case=TRUE))) { newBeer$Hoppy = 1 }
+        if(str_detect(beer.description, fixed("Floral", ignore_case=TRUE))) { newBeer$Floral = 1 }
+        if(str_detect(beer.description, fixed("Citrus", ignore_case=TRUE))) { newBeer$Citrus = 1 }
+        if(str_detect(beer.description, fixed("Milk", ignore_case=TRUE))) { newBeer$Milk = 1 }
+        if(str_detect(beer.description, fixed("Dark", ignore_case=TRUE))) { newBeer$Dark = 1 }
+        if(str_detect(beer.description, fixed("Creamy", ignore_case=TRUE))) { newBeer$Creamy = 1 }
+        if(str_detect(beer.description, fixed("Mouthfeel", ignore_case=TRUE))) { newBeer$Mouthfeel = 1 }
+        if(str_detect(beer.description, fixed("Soft", ignore_case=TRUE))) { newBeer$Soft = 1 }
+        if(str_detect(beer.description, fixed("Boozy", ignore_case=TRUE))) { newBeer$Boozy = 1 }
+        if(str_detect(beer.description, fixed("Caramel", ignore_case=TRUE))) { newBeer$Caramel = 1 }
+        if(str_detect(beer.description, fixed("Oatmeal", ignore_case=TRUE))) { newBeer$Oatmeal = 1 }
+        if(str_detect(beer.description, fixed("Dry", ignore_case=TRUE))) { newBeer$Dry = 1 }
+        if(str_detect(beer.description, fixed("Malty", ignore_case=TRUE))) { newBeer$Malty = 1 }
+        if(str_detect(beer.description, fixed("Piney", ignore_case=TRUE))) { newBeer$Piney = 1 }
+        if(str_detect(beer.description, fixed("Grassy", ignore_case=TRUE))) { newBeer$Grassy = 1 }
+        if(str_detect(beer.description, fixed("Tart", ignore_case=TRUE))) { newBeer$Tart = 1 }
+        if(str_detect(beer.description, fixed("Sour", ignore_case=TRUE))) { newBeer$Sour = 1 }
+        if(str_detect(beer.description, fixed("Salty", ignore_case=TRUE))) { newBeer$Salty = 1 }
+        if(str_detect(beer.description, fixed("Fruity", ignore_case=TRUE))) { newBeer$Fruity = 1 }
+        if(str_detect(beer.description, fixed("Heat", ignore_case=TRUE))) { newBeer$Heat = 1 }
+        if(str_detect(beer.description, fixed("Bitter", ignore_case=TRUE))) { newBeer$Bitter = 1 }
+        if(str_detect(beer.description, fixed("Coffee", ignore_case=TRUE))) { newBeer$Coffee = 1 }
+        if(str_detect(beer.description, fixed("Roasty", ignore_case=TRUE))) { newBeer$Roasty = 1 }
+        if(str_detect(beer.description, fixed("Chocolate", ignore_case=TRUE))) { newBeer$Chocolate = 1 }
+        if(str_detect(beer.description, fixed("Juicy", ignore_case=TRUE))) { newBeer$Juicy = 1 }
+        if(str_detect(beer.description, fixed("Funky", ignore_case=TRUE))) { newBeer$Funky = 1 }
+        if(str_detect(beer.description, fixed("Acidic", ignore_case=TRUE))) { newBeer$Acidic = 1 }
+        if(str_detect(beer.description, fixed("Crushable", ignore_case=TRUE))) { newBeer$Crushable = 1 }
+      }
+      print(newBeer)
+      beer.details <- rbind(beer.details, newBeer)
+      Sys.sleep(5)
+    },
+    error=function(e) {
+      message("Error in: ", beer.url) 
+      message(e)
+      row <- row + 1
+      Sys.sleep(5)
+    })
+  }
+  close_remote_chrome_driver(remDr)
+  write.csv(beer.details, file=paste("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/BeerDetails.csv", sep = ""), row.names = FALSE)
+  beer.details
+}
+
+
---
+++ data/Untappd_functions.R
@@ -0,0 +1,120 @@
+#
+# File contains commonly used functions for the case study
+#
+
+#
+# function to get a remote driver for chrome
+#
+get_remote_chrome_driver <- function() {
+  #
+  # Use RSelenium Package to navigate to multiple packages
+  # - Install RSelenium package from tar/zip file
+  # - Install Chrome driver
+  # - Need to start Selenium server: java -D"webdriver.chrome.driver"="c:\tools\chromedriver.exe" -jar .\selenium-server-standalone-3.13.0.jar 
+  #   before running the following code.
+  #
+  remDr <- remoteDriver(browserName="chrome")
+  remDr$open()
+  remDr$navigate(login.url)
+  remDr$findElement("id", "username")$sendKeysToElement(list(username))
+  remDr$findElement("id", "password")$sendKeysToElement(list(password))
+  remDr$findElement(using = "css selector", "span input")$clickElement()
+  remDr  
+}
+
+close_remote_chrome_driver <- function(remDr) {
+  remDr$close()
+}
+
+combine_beer_files <- function() {
+  # 
+  # Read the files
+  #
+  beer1 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails1-100.csv")
+  beer2 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails101-200.csv")
+  beer3 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails201-300.csv")
+  beer4 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails301-400.csv")
+  beer5 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails401-500.csv")
+  beer6 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails501-600.csv")
+  beer7 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails601-700.csv")
+  beer8 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails701-800.csv")
+  beer9 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails801-900.csv")
+  beer10 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails901-1000.csv")
+  beer11 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails1001-1100.csv")
+  beer12 <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/temp/BeerDetails1101-1176.csv")
+  
+  combined.beers <- rbind(beer1, beer2)
+  combined.beers <- rbind(combined.beers, beer3)
+  combined.beers <- rbind(combined.beers, beer4)
+  combined.beers <- rbind(combined.beers, beer5)
+  combined.beers <- rbind(combined.beers, beer6)
+  combined.beers <- rbind(combined.beers, beer7)
+  combined.beers <- rbind(combined.beers, beer8)
+  combined.beers <- rbind(combined.beers, beer9)
+  combined.beers <- rbind(combined.beers, beer10)
+  combined.beers <- rbind(combined.beers, beer11)
+  combined.beers <- rbind(combined.beers, beer12)
+  
+  write.csv(combined.beers, file="C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/MasterBeerList.csv", row.names = FALSE)
+}
+
+combine_flavor_profiles <- function() {
+  
+  beers.master <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/MasterBeerList.csv", stringsAsFactors = FALSE)
+  user.profile <- read.csv("C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/MasterUserProfile.Original.csv", stringsAsFactors = FALSE)
+  
+  # https://www.thebrewenthusiast.com/ibus/ says beers will have a rating of 5-120 IBU
+  # we will replace beers with 'No' IBUs to 5
+  beers.master$IBU = replace(beers.master$IBU, beers.master$IBU=='No', 5)
+  
+  for(row in 1:nrow(user.profile)) {
+    
+    #print(row)
+    user.checkin <- user.profile[row,]
+    beer.record <- beers.master[(beers.master$BeerId==user.checkin$BeerId),]
+    
+    #print(user.checkin)
+    if(beer.record$Light == 1) { user.checkin$Light = 1 }
+    if(beer.record$Smooth == 1) { user.checkin$Smooth = 1 }
+    if(beer.record$Clean == 1) { user.checkin$Clean = 1 }
+    if(beer.record$Sweet == 1) { user.checkin$Sweet = 1 }
+    if(beer.record$Woody == 1) { user.checkin$Woody = 1 }
+    if(beer.record$Zippy == 1) { user.checkin$Zippy = 1 }
+    if(beer.record$Strong == 1) { user.checkin$Strong = 1 }
+    if(beer.record$Hoppy == 1) { user.checkin$Hoppy = 1 }
+    if(beer.record$Floral == 1) { user.checkin$Floral = 1 }
+    if(beer.record$Citrus == 1) { user.checkin$Citrus = 1 }
+    if(beer.record$Milk == 1) { user.checkin$Milk = 1 }
+    if(beer.record$Dark == 1) { user.checkin$Dark = 1 }
+    if(beer.record$Creamy == 1) { user.checkin$Creamy = 1 }
+    if(beer.record$Mouthfeel == 1) { user.checkin$Mouthfeel = 1 }
+    if(beer.record$Boozy == 1) { user.checkin$Boozy = 1 }
+    if(beer.record$Caramel == 1) { user.checkin$Caramel = 1 }
+    if(beer.record$Oatmeal == 1) { user.checkin$Oatmeal = 1 }
+    if(beer.record$Dry == 1) { user.checkin$Dry = 1 }
+    if(beer.record$Malty == 1) { user.checkin$Malty = 1 }
+    if(beer.record$Piney == 1) { user.checkin$Piney = 1 }
+    if(beer.record$Grassy == 1) { user.checkin$Grassy = 1 }
+    if(beer.record$Tart == 1) { user.checkin$Tart = 1 }
+    if(beer.record$Sour == 1) { user.checkin$Sour = 1 }
+    if(beer.record$Salty == 1) { user.checkin$Salty = 1 }
+    if(beer.record$Fruity == 1) { user.checkin$Fruity = 1 }
+    if(beer.record$Heat == 1) { user.checkin$Heat = 1 }
+    if(beer.record$Bitter == 1) { user.checkin$Bitter = 1 }
+    if(beer.record$Coffee == 1) { user.checkin$Coffee = 1 }
+    if(beer.record$Roasty == 1) { user.checkin$Roasty = 1 }
+    if(beer.record$Chocolate == 1) { user.checkin$Chocolate = 1 }
+    if(beer.record$Juicy == 1) { user.checkin$Juicy = 1 }
+    if(beer.record$Funky == 1) { user.checkin$Funky = 1 }
+    if(beer.record$Acidic == 1) { user.checkin$Acidic = 1 }
+    if(beer.record$Crushable == 1) { user.checkin$Crushable = 1 }
+    #print(user.checkin)
+    
+    user.checkin$IBU = beer.record$IBU
+    user.checkin$ABV = beer.record$ABV
+    user.checkin$Style = beer.record$Style
+      
+    user.profile[row,] <- user.checkin
+  }
+  write.csv(user.profile, file="C:/SMU/Courses/MSDS 6306 - DoingDataScience/github/MSDS_CaseStudy2/data/MasterUserProfile.csv", row.names = FALSE)
+}
