b70cfe09edd52ad2e5ba9c510d2cb333b1ea3089
Chris <chris.ballenger@gmail.com>
2018-08-07 18:20:04 -0500

    include beer categories

--- ParseCheckins.R
+++ ParseCheckins.R
@@ -2,6 +2,7 @@ setwd("/Users/christopherballenger/Documents/Data Science/MSDS 6306/Projects/Cas
 
 library(jsonlite)
 library(plyr)
+library(dplyr)
 source("Untappd_api.R")
 
 result <- data.frame()
@@ -19,14 +20,16 @@ for(i in 1:length(filterObjects) ){
     print( paste(i, "-", dim(result)[1] ) )
 }
 
+dim(result)
+
 #remove duplicates
 result.distinct <- unique(result)
-
-result.distinct.count <- count(result.distinct$checkinId)
-result.distinct <- merge(result.distinct, result.distinct.count, by.x="checkinId", by.y="x")
-
+names(result.distinct)
+result.distinct.count <- plyr::count(result.distinct$checkinId)
+result.distinct <- dplyr::inner_join(result.distinct, result.distinct.count, by=c("checkinId"="x"))
+dim(result.distinct)
 result.distinct <- result.distinct[result.distinct$freq == 1,]
-
+dim(result.distinct)
 #Identify if checkin was at brewery
 breweryIdToVenueId <- data.frame(
     breweryId = c(48372,29815,11028,185850,45011,134551,13688), 
@@ -34,17 +37,24 @@ breweryIdToVenueId <- data.frame(
     isBreweryLocation = rep("Y",7)
 )
 
-result.distinct <- merge(result.distinct, breweryIdToVenueId, by.x = c("breweryId","venueId"), by.y = c("breweryId","venueId"), all.x = TRUE)
+result.distinct <- left_join(
+    result.distinct,
+    breweryIdToVenueId, 
+    by = c("breweryId" = "breweryId", "venueId" = "venueId" )
+)
+dim(result.distinct)
 result.distinct$isBreweryLocation <- as.character(result.distinct$isBreweryLocation)
 result.distinct[is.na(result.distinct$isBreweryLocation)&!is.na(result.distinct$venueId),30] <- "N"
 
-
-# str(result.distinct[is.na(result.distinct$isBreweryLocation),])
-
-# result.distinct <- result.distinct[,]
-# str(result.distinct)
+# Simplfy Beer Styles with a BeerCategory
+beerCategories <- read.csv("beerstyle.csv", stringsAsFactors = FALSE)
+# Remove Factors
+result.distinct$beerStyle <- as.character(result.distinct$beerStyle)
+result.distinct <- left_join(result.distinct, beerCategories, by = c("beerStyle" = "beerStyle"))
+dim(result.distinct)
 
 # Save Data
+names(result.distinct)
 write.csv(result.distinct, "data/checkins.csv", row.names=FALSE)
 
 
@@ -55,23 +65,27 @@ distinctBeerIds <- beerIds[order(beerIds$freq, decreasing = F),]
 
 # Get min \ max checkin details for future checkin data gathering
 maxIds <- aggregate( result.distinct$checkinId, list( result.distinct$beerId ), max )
+names(maxIds) <- c("beerId","maxId")
 minIds <- aggregate( result.distinct$checkinId, list( result.distinct$beerId ), min )
+names(minIds) <- c("beerId","minId")
 lastCreatedDate <- aggregate( 
     strptime( result.distinct$createdAt, "%a, %d %b %Y %T"),
     list(result.distinct$beerId ), 
     max,
     na.rm=TRUE 
 )
+names(lastCreatedDate) <- c("beerId","lastCreatedDate")
 
-str(result.distinct)
+names(result.distinct)
 
-breweries <- unique(result.distinct[,2:10])
+breweries <- unique(result.distinct[,1:8])
 str(breweries)
-breweries <- merge(breweries, distinctBeerIds, by.x = "beerId", by.y = "beerId" )
-breweries <- merge(breweries, maxIds, by.x = "beerId", by.y = "Group.1")
-breweries <- merge(breweries, minIds, by.x = "beerId", by.y = "Group.1")
-breweries <- merge(breweries, lastCreatedDate, by.x = "beerId", by.y = "Group.1")
+breweries <- inner_join(breweries, distinctBeerIds, by = c("beerId" = "beerId") )
+breweries <- inner_join(breweries, maxIds, by = c("beerId" = "beerId") )
+breweries <- inner_join(breweries, minIds, by = c("beerId" = "beerId") )
+breweries <- inner_join(breweries, lastCreatedDate, by = c("beerId" = "beerId") )
 
+names(breweries)
 write.csv(breweries, "data/breweries.csv",row.names = FALSE)
 
 head(breweries)
@@ -93,3 +107,5 @@ head(breweries)
 dim(result.distinct)
 
 length(result.distinct$checkinId) - length( unique(result.distinct$checkinId) )
+
+
