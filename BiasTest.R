d97a7cec331bd4d8454e1b9206c5c712897e1582
Chris <chris.ballenger@gmail.com>
2018-08-13 20:38:49 -0500

    Location Bias Test

--- BiasTest.R
+++ BiasTest.R
@@ -2,6 +2,7 @@ setwd("/Users/christopherballenger/Documents/Data Science/MSDS 6306/Projects/Cas
 
 library(jsonlite)
 library(plyr)
+library(dplyr)
 source("env.R")
 source("Untappd_api.R")
 
@@ -10,11 +11,15 @@ checkins <- read.csv("data/checkins.csv")
 
 # remove no venue checkins
 checkins.filter <- checkins[!is.na(checkins$isBreweryLocation),]
-
-# remove 0 rating scores
 checkins.filter <- checkins.filter[checkins.filter$ratingScore != 0,]
-
 dim(checkins.filter)
+
+# reduce columes
+names(checkins.filter)
+checkins.final <- checkins.filter[,c(9,10,11,13,15,18,20,31,32)]
+head(checkins.final)
+
+checkins.category <- plyr::count(checkins.filter[!is.na(checkins.filter$venueParentCategory),]$venueParentCategory)
 
 hist(checkins.filter$ratingScore)
 qqnorm(checkins.filter$ratingScore, main='Rating Scores')
@@ -40,7 +45,7 @@ length(checkins.filter.deepellum[checkins.filter.deepellum$isBreweryLocation=="N
 length(checkins.filter.deepellum[checkins.filter.deepellum$isBreweryLocation=="Y",]$ratingScore)
 count(checkins.filter.deepellum$ratingScore)
 
-breweryIds <- unique(checkins.filter[,c(1,4)])
+breweryIds <- unique(checkins.filter[,1:2])
 for(i in 1:length(breweryIds$breweryId)){
     ttest <- t.test(
         ratingScore ~ isBreweryLocation,
@@ -57,4 +62,78 @@ ttest <- t.test(
 )
 ttest
 
-read.csv("beerstyle.csv")
+# count how many checkins per beer
+distinctBeerIds <- plyr::count(checkins.filter, "beerId")
+names(distinctBeerIds) <- c("beerId","totalCheckins")
+checkins.filter <- inner_join(checkins.filter, distinctBeerIds, by = c("beerId" = "beerId") )
+
+breweryIds <- unique(checkins.filter[,1:2])
+for(i in 1:length(breweryIds$breweryId)){
+    ttest <- t.test(
+        ratingScore ~ isBreweryLocation,
+        data=checkins.filter[checkins.filter$totalCheckins >=100 & checkins.filter$breweryId==breweryIds[i,1],],
+        var.equal=T, conf.level=0.95
+    )
+    print( paste( breweryIds[i,2], "-", breweryIds[i,1], "-", print(ttest$p.value, digits = 18), sep=" "))
+}
+
+# Accounting for independent
+# creating an avg for users who rated multiple times
+checkins.filter %>% count(userId, beerId, isBreweryLocation, sort = TRUE)
+
+checkins.filter.independent <- checkins.filter %>% 
+    group_by(breweryId, breweryName, userId, beerId, isBreweryLocation) %>% 
+    summarise(
+        iRatingScore = mean(ratingScore)
+    )
+
+checkins.filter.independent <- as.data.frame(checkins.filter.independent)
+# checkins.filter.independent %>% count(breweryId, isBreweryLocation, sort = TRUE)
+distinctBeerIds <- plyr::count(checkins.filter.independent, "beerId")
+names(distinctBeerIds) <- c("beerId","totalCheckins")
+checkins.filter.independent <- inner_join(checkins.filter.independent, distinctBeerIds, by = c("beerId" = "beerId") )
+
+checkins.filter.independent[checkins.filter.independent$totalCheckins>=10,] %>% count(breweryName, isBreweryLocation, sort = F)
+
+breweryIds <- unique(checkins.filter.independent[,1:2])
+breweries <- data.frame(
+    breweryName = character(),
+    t.value = numeric(),
+    p.value = numeric(),
+    Brewery.Rating = numeric(),
+    Not.At.Brewery.Rating = numeric(),
+    avg.difference = numeric(),
+    conf.int.lwr = numeric(),
+    conf.int.upr = numeric()
+)
+for(i in 1:length(breweryIds$breweryId)){
+    test.data <- checkins.filter.independent[ checkins.filter.independent$totalCheckins >= 10 & checkins.filter.independent$breweryId==breweryIds[i,1],]
+    test.data <- test.data[order(test.data$isBreweryLocation, decreasing = T),]
+    ttest <- t.test(
+        iRatingScore ~ isBreweryLocation,
+        data=test.data,
+        var.equal=T, 
+        conf.level=0.95,
+        alternative = "less"
+    )
+    ttest.conf.int <- t.test(
+        iRatingScore ~ isBreweryLocation,
+        data=test.data,
+        var.equal=T, 
+        conf.level=0.90,
+        alternative = "two.sided"
+    )
+    
+    breweries <- rbind( breweries, data.frame(
+        breweryName = c(as.character(test.data[1,]$breweryName)),
+        t.value = c(ttest$statistic),
+        p.value = c(round(ttest$p.value,5)),
+        Brewery.Rating = c(ttest$estimate[2]),
+        Not.At.Brewery.Rating = c(ttest$estimate[1]),
+        avg.difference = c(ttest$estimate[2] - ttest$estimate[1]),
+        conf.int.lwr = c(ttest.conf.int$conf.int[2]*-1),
+        conf.int.upr = c(ttest.conf.int$conf.int[1]*-1)
+    ))
+}
+breweries
+
