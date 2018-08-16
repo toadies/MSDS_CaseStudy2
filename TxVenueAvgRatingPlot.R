99391eca1f74878055bbec33058f445c331fc091
Chris <chris.ballenger@gmail.com>
2018-08-16 15:17:28 -0500

    Q1 edits

--- TxVenueAvgRatingPlot.R
+++ TxVenueAvgRatingPlot.R
@@ -3,8 +3,10 @@ library(dplyr)
 library(sqldf)
 library(geojsonio)
 library(ggmap)
+# install.packages("ggmap")
+# setwd("C:/Users/Anand/Documents/SMU/DDS/GITHUB/Case-Study-2/MSDS_CaseStudy2/")
+setwd("/Users/christopherballenger/Documents/Data Science/MSDS 6306/Projects/CaseStudy2")
 
-setwd("C:/Users/Anand/Documents/SMU/DDS/GITHUB/Case-Study-2/MSDS_CaseStudy2/")
 filePath='data/checkins.csv'
 checkIn_all <- read.csv(filePath)
 summary(checkIn_all)
@@ -30,31 +32,47 @@ head(txVenues)
 geocode("Texas")
 map_tx = get_map(location=c(lon=-99.90181,lat=31.9686),maptype = "terrain", zoom = 14)
 ggmap(map_tx)
+data_json <- geojson_read("https://raw.githubusercontent.com/TNRIS/tx.geojson/master/tx.geojson",what="sp")
 
 p <- ggplot()+
 ggtitle("Texas Venues Average Rating score")+ 
-xlab("Venue Longitude")+
-ylab("Venue Latitude")
+xlab("")+
+ylab("")
 p <- p +theme(
-  plot.title = element_text(color="red", size=14, face="bold.italic",hjust=0,vjust=1),
-  axis.title.x = element_text(color="blue", size=14, face="bold"),
-  axis.title.y = element_text(color="#993333", size=14, face="bold")
+    plot.background = element_blank()
+    plot.title = element_text(color="red", size=14, face="bold.italic",hjust=0,vjust=1),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    rect = element_blank()
 )
-p <- p + geom_polygon( data=data_json,aes(x=long, y=lat, group = group),colour="black",fill="grey" )
+p <- p + geom_polygon( 
+    data=data_json,
+    aes(x=long, y=lat, group = group),
+    colour="black",
+    fill="grey" )
 p
 
 
-p<- p+ geom_point(aes(x = venueLng, y = venueLat,alpha=3/7,color=cut(meanRS, c(-Inf,0,1,2,3,4,5,Inf))), data = txVenues,shape=18,size=2,position = position_jitter(w=0.002,h=0.002))
+# p<- p+ geom_point(aes(x = venueLng, y = venueLat,alpha=3/7,color=cut(meanRS, c(-Inf,0,1,2,3,4,5,Inf))), data = txVenues,shape=18,size=2,position = position_jitter(w=0.002,h=0.002))
+p<- p+ geom_point(
+    aes(x = venueLng, y = venueLat,color=meanRS), 
+    data = txVenues,
+    shape=18,
+    size=2,
+    position = position_jitter(w=0.002,h=0.002)
+)
+p <- p + scale_color_gradient(low="#C0FAAB",high="#388A19")
 p
-p <- p +scale_color_manual(name = "Rating Score Scale",
-                           values = c("(-Inf,0]" = "black",
-                                      "(0,1]" = "coral1",
-                                      "(1,2]" = "red",
-                                      "(2,3]" = "green",
-                                      "(3,4]" = "blue",
-                                      "(4,5]" = "yellow",
-                                      "(5, Inf]" = "violet"),
-                           labels = c("<= 0", "0 to 1", "1 to 2","2 to 3","3 to 4","4 to 5"," >=5"))
+# p <- p +scale_color_manual(name = "Rating Score Scale",
+#                            values = c("(-Inf,0]" = "black",
+#                                       "(0,1]" = "coral1",
+#                                       "(1,2]" = "red",
+#                                       "(2,3]" = "green",
+#                                       "(3,4]" = "blue",
+#                                       "(4,5]" = "yellow",
+#                                       "(5, Inf]" = "violet"),
+#                            labels = c("<= 0", "0 to 1", "1 to 2","2 to 3","3 to 4","4 to 5"," >=5"))
 p
 
 ggsave("TexasPlot.png",p)
